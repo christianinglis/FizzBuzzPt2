bbca0b7f49e79c4254b9d03dcec23fa49a7202eb
Christian Inglis <ccinglis@my.waketech.edu>
2026-02-19 13:30:14 -0500

    update

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,7 @@
 // TODO: Define constants for the form and the result display area
-let userinput = document.getElementById("numberInput");
-let submit = document.getElementById("submitbutton");
-let result = document.getElementById("result");
+const userinput = document.getElementById("numberInput");
+const submit = document.getElementById("submitbutton");
+const result = document.getElementById("result");
 
 
 // TODO: Add the first line of the event listener to handle form submission
@@ -11,29 +11,24 @@ function submitfunction(event) {
 
 
 // TODO: Get the number from the form input
-let inputnum = Number(userinput.value);
+    let inputnum = Number(userinput.value);
 // TODO: Write FizzBuzz logic here
-if (inputnum % 15 === 0) {
-    output = "FizzBuzz";
-    return output;
-}
-else if (inputnum % 3 == 0) {
-    output = "Fizz"
-    return output;
-}
-else if (inputnum % 5 === 0) {
-    output = "Buzz"
-    return output;
-}
-else if (isNaN(inputnam)) {
-    output = "Enter a number";
-    return output;
-}
-else {
-    output = inputnum;
-    return output;
-}
-}
+    if (inputnum % 15 === 0) {
+        result.textContent = "FizzBuzz";
+    }
+    else if (inputnum % 3 == 0) {
+        result.textContent = "Fizz";
+    }
+    else if (inputnum % 5 === 0) {
+        result.textContent = "Buzz";
+    }
+    else if (isNaN(inputnam)) {
+        result.textContent = "Enter a number";
+    }
+    else {
+        output = inputnum;
+        return output;
+    }   
 
 // If divisible by both 3 and 5, set the output to "FizzBuzz"
 // If divisible by 3, set the output to "Fizz"
