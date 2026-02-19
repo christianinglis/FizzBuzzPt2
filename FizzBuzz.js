009582e00a1df42b1f9873a072cbb076307c370c
Christian Inglis <ccinglis@my.waketech.edu>
2026-02-19 13:10:38 -0500

    middle

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,36 @@
 // TODO: Define constants for the form and the result display area
+let userinput = document.getElementById("numberInput");
+let submit = document.getElementById("submitbutton");
+let result = document.getElementById("result");
+
 
 // TODO: Add the first line of the event listener to handle form submission
-
-    // Prevent the form from refreshing the page
+submit.addEventListener("click", submitfunction);
+function submitfunction(event) {
     event.preventDefault();
 
-// TODO: Get the number from the form input
 
+// TODO: Get the number from the form input
+let inputnum = Number(userinput.value);
 // TODO: Write FizzBuzz logic here
+if (inputnum % 15 === 0) {
+    output = "FizzBuzz";
+    return output;
+}
+else if (inputnum % 3 == 0) {
+    output = "Fizz"
+    return output;
+}
+else if (inputnum % 5 === 0) {
+    output = "Buzz"
+    return output;
+}
+else {
+    output = inputnum;
+    return output;
+}
+}
+
 // If divisible by both 3 and 5, set the output to "FizzBuzz"
 // If divisible by 3, set the output to "Fizz"
 // If divisible by 5, set the output to "Buzz"
